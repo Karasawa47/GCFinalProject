81acdf50875a34368eb7c0f73726c2539f4425d2
mwerner340 <mikewerner@att.net>
2014-08-04 15:36:56 -0400

    Added Events Button and changed view comment controller

--- GCFinalProject/GCFinalProject/Models/Comment.cs
+++ GCFinalProject/GCFinalProject/Models/Comment.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+
 
 namespace GCFinalProject.Models
 {
     public class Comment
     {
+        
         public int CommentID { get; set; }
         public int EventID { get; set; }
         public DateTime Date { get; set; }
+        [Display(Name = "Comments")] 
         public string CommentText { get; set; }
         
     }
