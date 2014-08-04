5ff0f250ae9624c9081fbb4b887ec1eaebffc22f
Karasawa47 <karasawa47@gmail.com>
2014-08-04 14:10:29 -0400

    Added EventSiteContext

---
+++ GCFinalProject/GCFinalProject/DAL/EventSiteContext.cs
@@ -0,0 +1,28 @@
+﻿using GCFinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+
+namespace GCFinalProject.DAL
+{
+    public class EventSiteContext :DbContext
+    {
+        public EventSiteContext()
+            : base("EventSiteContext")
+        {
+
+        }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
