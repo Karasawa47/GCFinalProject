45c5da1b2961bc5f36f394d05735aa45176fc8b2
mwerner340 <mikewerner@att.net>
2014-08-05 15:39:09 -0400

    Added content to about page

--- GCFinalProject/GCFinalProject/Controllers/HomeController.cs
+++ GCFinalProject/GCFinalProject/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace GCFinalProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Downtown Detroit is a year-round destination for sports and entertainment. The Tigers, Lions, and Red Wings all call downtown Detroit home. The city also boasts top-notch venues for live entertainment, including the Fox Theatre, Detroit Opera House, The Fillmore Detroit, Gem Theatre, and Music Hall. Parts of the city's riverfront have been revitalized in recent years, adding the Detroit RiverWalk and William G. Milliken State Park and Harbor. During the summer, Chene Park and Hart Plaza welcome visitors to the riverfront area for concerts, festivals and other events. Detroit Events is striving to provide  Detroit visitors with the most up to date event schedule";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Detroit Events";
 
             return View();
         }
