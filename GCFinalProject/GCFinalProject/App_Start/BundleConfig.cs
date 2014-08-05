9afb5edaa411ac4be36a4ceb08a7e39924f2c65e
mwerner340 <mikewerner@att.net>
2014-08-05 11:01:02 -0400

    Bootstrap added and Event buttons removed

--- GCFinalProject/GCFinalProject/App_Start/BundleConfig.cs
+++ GCFinalProject/GCFinalProject/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace GCFinalProject
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-style1.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
