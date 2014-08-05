a23ac21201c25e05265a7de15d9ef29245d3051c
Karasawa47 <karasawa47@gmail.com>
2014-08-05 08:46:00 -0400

    Migration to add time fields, added Event Controller

--- GCFinalProject/GCFinalProject/Models/Event.cs
+++ GCFinalProject/GCFinalProject/Models/Event.cs
@@ -9,23 +9,46 @@ namespace GCFinalProject.Models
     public class Event
     {
         public int EventID { get; set; }
+
         [Display(Name = "Event Description")]
         public string EventDesc { get; set; }
+
         [Display(Name="Event Name")]
         public string EventName { get; set; }
+        
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Start Date and Time")]
         public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "End Date and Time")]
         public DateTime EndDate { get; set; }
+
+        [DataType(DataType.Time)]
+        [Display(Name = "Start Time")]
+        public DateTime StartTime { get; set; }
+
+        [DataType(DataType.Time)]
+        [Display(Name = "End Time")]
+        public DateTime EndTime { get; set; }
+
         public bool IsActive { get; set; }
+
         [Display(Name = "Event Contact")]
         public string ContactInfo { get; set; }
+
         [Display(Name = "Link URL")]
         public string LinkUrl { get; set; }
+
         [Display(Name = "Image URL")]
         public string ImageUrl { get; set; }
+
         public int CategoryID { get; set; }
+
         public string Location { get; set; }
+
         [Display(Name = "Short Description")]
         public string  ShortSummary { get; set; }
 
