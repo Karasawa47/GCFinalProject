ed5129309d62eff12bd02fd60bb64e53eefc503c
4ngardner <nicole.gardner@outlook.com>
2014-08-11 15:21:15 -0400

    Pictures links are routed
    Routed clickable picture buttons to correct search on events

--- GCFinalProject/GCFinalProject/Controllers/HomeController.cs
+++ GCFinalProject/GCFinalProject/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
+
 namespace GCFinalProject.Controllers
 {
     public class HomeController : Controller
